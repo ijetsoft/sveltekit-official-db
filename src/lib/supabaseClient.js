e8410440bd29f242303e257712904460b9b22edd
master <ijetsoft@gmail.com>
2023-04-30 19:12:24 +0300

    project2

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-  export const supabase = createClient('https://cmlbjheksqwrsatzaohf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtbGJqaGVrc3F3cnNhdHphb2hmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI1MDAzMTcsImV4cCI6MTk5ODA3NjMxN30.T0TfmLGLtodwoW9FCYw4dD7oD_9zqHIWdBCdVh22Wxg')
+  export const supabase = createClient('https://itbjijzuoedaqbjawrgd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0Ymppanp1b2VkYXFiamF3cmdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI4Njk3NTUsImV4cCI6MTk5ODQ0NTc1NX0.a2dyRN7eAWl_YizGhywfL8htXp7bpPVYMWYu59Z_N9A')
